190add842c5c907b4d579e8142ad79a8792aece4
Evgen Cheban <don.evgen86@gmail.com>
2022-08-31 14:08:28 +0300

    first slider

---
+++ script.js
@@ -0,0 +1,55 @@
+'use strict'
+
+let images = document.querySelectorAll('.slider img');
+let corrent = images.length -1;
+
+function slider () {
+    for (let i = 0; i < images.length; i++) {
+        images[i].classList.add('opacity_0');
+    }
+    images[corrent].classList.remove('opacity_0');
+}
+
+// мой
+
+document.querySelector('.button_slider_box_right button').onclick = function() {
+    if (corrent +1 == images.length) {
+        corrent = 0;
+    } else {
+        corrent ++;
+    }
+    slider();
+};
+
+// чужой
+
+// document.querySelector('.button_slider_box_right button').onclick = function() {
+//     if (corrent +1 == images.length) {
+//         corrent = 0;
+//     } else {
+//         corrent ++;
+//     }
+//     slider();
+// };
+
+// мой
+
+document.querySelector('.button_slider_box_left button').onclick = function() {
+    if (corrent -1 == -1) {
+        corrent = images.length -1;
+    } else {
+        corrent --;
+    }
+    slider();
+};
+
+// чужой
+
+// document.querySelector('.button_slider_box_left button').onclick = function() {
+//     if (corrent -1 == -1) {
+//         corrent = images.length -1;
+//     } else {
+//         corrent --;
+//     }
+//     slider();
+// };
